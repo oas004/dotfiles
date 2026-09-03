dac079e547062ed6871051dd5353b4c54f818ee8
Odin Asbjørnsen <odinas004@gmail.com>
2026-09-03 15:46:23 +0200

    Fix treesitter textobjects for new API (af, if, ac, ic)

--- lua/plugins/parser.lua
+++ lua/plugins/parser.lua
@@ -2,71 +2,77 @@ return {
     -- https://github.com/nvim-treesitter/nvim-treesitter
     {
         "nvim-treesitter/nvim-treesitter",
-        lazy = false,  -- Load immediately, don't lazy load
-        priority = 100,  -- Load early but after core plugins
+        lazy = false,
+        priority = 100,
         build = ":TSUpdate",
         config = function()
             require("nvim-treesitter.configs").setup({
-                -- A list of parser names, or "all" (the five listed parsers should always be installed)
-                -- Reference: https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
                 ensure_installed = {
-                    -- required
-                    "c",
-                    "lua",
-                    "vim",
-                    "vimdoc",
-                    "query",
-
-                    -- optional
-                    "go",
-                    "gomod",
-                    "gosum",
-                    "gowork",
-                    "javascript",
-                    "json",
-                    "kotlin",
-                    "typescript",
-                    "yaml",
+                    "c", "lua", "vim", "vimdoc", "query",
+                    "go", "gomod", "gosum", "gowork",
+                    "javascript", "json", "kotlin", "typescript", "yaml",
                 },
-                textobjects = { "nvim-treesitter/nvim-treesitter-textobjects" },
-                -- Install parsers synchronously (only applied to `ensure_installed`)
                 sync_install = false,
-
-                -- Automatically install missing parsers when entering buffer
-                -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
                 auto_install = false,
-
                 highlight = {
                     enable = true,
                     additional_vim_regex_highlighting = false,
                     disable = function(lang, buf)
-                        -- Disable treesitter for large files
-                        local max_filesize = 100 * 1024 -- 100 KB
+                        local max_filesize = 100 * 1024
                         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
                         if ok and stats and stats.size > max_filesize then
                             return true
                         end
                     end,
                 },
-                textobjects = {
-                    move = {
-                      enable = true,
-                      set_jumps = true,
-                      goto_next_start = { ["]m"] = "@function.outer" },
-                      goto_previous_start = { ["[m"] = "@function.outer" },
-                      goto_next_end = { ["]M"] = "@function.outer" },
-                      goto_previous_end = { ["[M"] = "@function.outer" },
-                    },
-                    select = {
-                      enable = true,
-                      lookahead = true,
-                      keymaps = {
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                      },
-                    },
-                }
             })
+        end,
+    },
+
+    -- Treesitter text objects (af, if, etc)
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            require("nvim-treesitter-textobjects").setup({
+                select = {
+                    lookahead = true,
+                },
+            })
+
+            -- Text object keymaps: af = around function, if = inside function
+            vim.keymap.set({ "x", "o" }, "af", function()
+                require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+            end, { desc = "Select around function" })
+
+            vim.keymap.set({ "x", "o" }, "if", function()
+                require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+            end, { desc = "Select inside function" })
+
+            vim.keymap.set({ "x", "o" }, "ac", function()
+                require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
+            end, { desc = "Select around class" })
+
+            vim.keymap.set({ "x", "o" }, "ic", function()
+                require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
+            end, { desc = "Select inside class" })
+
+            -- Move keymaps: ]m = next function, [m = previous function
+            vim.keymap.set({ "n", "x", "o" }, "]m", function()
+                require("nvim-treesitter-textobjects.move").goto_next_start("@function.outer", "textobjects")
+            end, { desc = "Next function start" })
+
+            vim.keymap.set({ "n", "x", "o" }, "[m", function()
+                require("nvim-treesitter-textobjects.move").goto_previous_start("@function.outer", "textobjects")
+            end, { desc = "Previous function start" })
+
+            vim.keymap.set({ "n", "x", "o" }, "]M", function()
+                require("nvim-treesitter-textobjects.move").goto_next_end("@function.outer", "textobjects")
+            end, { desc = "Next function end" })
+
+            vim.keymap.set({ "n", "x", "o" }, "[M", function()
+                require("nvim-treesitter-textobjects.move").goto_previous_end("@function.outer", "textobjects")
+            end, { desc = "Previous function end" })
         end,
     },
 }
